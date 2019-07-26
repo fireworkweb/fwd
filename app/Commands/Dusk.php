e9452890dec166fd4be94dc65b254ef8f1895340
Filipe Bojikian Rissi <f.b.rissi@gmail.com>
2019-07-26 10:34:28 -0300

    Prepare dusk to receive changed files to find and filter the test the specific files

--- app/Commands/Dusk.php
+++ app/Commands/Dusk.php
@@ -2,10 +2,14 @@
 
 namespace App\Commands;
 
-use App\Process;
 use App\Commands\Traits\ArtisanCall;
 use App\Commands\Traits\HasDynamicArgs;
+use App\Process;
+use Brick\Reflection\ImportResolver;
+use Illuminate\Support\Collection;
 use LaravelZero\Framework\Commands\Command;
+use ReflectionClass;
+use Symfony\Component\Finder\Finder;
 
 class Dusk extends Command
 {
@@ -17,6 +21,15 @@ class Dusk extends Command
      * @var string
      */
     protected $name = 'dusk';
+
+    /**
+     * The signature of the command.
+     *
+     * @var string
+     */
+    protected $signature = 'dusk
+                            {--f|files= : List of files to find respective test separated by space}
+                            {--d|dir : Test files directory}';
 
     /**
      * The description of the command.
@@ -32,6 +45,57 @@ class Dusk extends Command
      */
     public function handle(Process $process)
     {
-        return $process->asFWDUser()->dockerComposeExec('app php artisan dusk', $this->getArgs());
+        $changedFiles = collect(explode(' ', $this->option('files')))
+            ->map(function ($file) {
+                include($file);
+
+                return end(get_declared_classes());
+            });
+
+        if (! $changedFiles) {
+            return $process->asFWDUser()->dockerComposeExec('app php artisan dusk', $this->getArgs());
+        }
+
+        $path = base_path($this->option('dir'));
+        $files = Finder::create()
+            ->files()
+            ->in($path)
+            ->name('*.php');
+
+        $filter = collect($files)
+            ->map(function ($file) {
+                include($file);
+
+                return end(get_declared_classes());
+            })
+            ->filter(function ($class) use ($changedFiles) {
+
+                return $this->getClassAnnotations($class)
+                    ->reduce(function ($carry, $item) use ($changedFiles) {
+                        return $carry || $changedFiles->contains($item);
+                    });
+            })
+            ->join(' ');
+
+        return $process->asFWDUser()->dockerComposeExec('app php artisan dusk',
+            array_merge(
+                $this->getArgs(),
+                ['--filter' => $filter]
+            )
+        );
+    }
+
+    protected function getClassAnnotations($class): Collection
+    {
+        $class = new ReflectionClass($class);
+        $resolver = new ImportResolver($class);
+        $comment_string = $class->getDocComment();
+
+        preg_match_all('#(?:@see\s*)(?P<class>[a-zA-Z0-9, ()_].*)#', $comment_string, $matches, PREG_PATTERN_ORDER);
+
+        return collect($matches['class'])
+            ->map(function ($class) use ($resolver) {
+                return $resolver->resolve($class);
+            });
     }
 }
