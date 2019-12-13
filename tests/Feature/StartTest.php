77b6d9c299870d045d03159efbe2614647b1fedd
fabriciojs <fajosesouza@gmail.com>
2019-12-13 02:04:11 -0300

    Fix Style

--- tests/Feature/StartTest.php
+++ tests/Feature/StartTest.php
@@ -13,7 +13,7 @@ class StartTest extends TestCase
 
         $this->artisan('start')->assertExitCode(0);
 
-        $this->assertDockerCompose('up -d '.env('FWD_START_DEFAULT_SERVICES'));
+        $this->assertDockerCompose('up -d ' . env('FWD_START_DEFAULT_SERVICES'));
     }
 
     public function testStartWithAll()
