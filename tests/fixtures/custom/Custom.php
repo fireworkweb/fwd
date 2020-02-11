dfafbdb5ccb6b66c265d8fa711feaf554886d955
Daniel Polito <danielbpolito@gmail.com>
2020-02-11 18:54:01 -0300

    Fix Test for good

---
+++ tests/fixtures/custom/Custom.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Builder\Builder;
+use App\Commands\Command;
+
+class Custom extends Command
+{
+    /**
+     * The name of the command.
+     *
+     * @var string
+     */
+    protected $name = 'custom';
+
+    /**
+     * The description of the command.
+     *
+     * @var string
+     */
+    protected $description = 'custom command';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        return $this->commandExecutor->run(
+            Builder::make('echo custom')
+        );
+    }
+}
